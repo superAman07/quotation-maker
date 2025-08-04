dca6ce72d5134cbbefeedbe3405124ad6e307e47
superAman07 <rivalaman123@gmail.com>
2025-08-04 15:48:48 +0530

    updated cols to 4 from 5

--- components/ServiceCards.tsx
+++ components/ServiceCards.tsx
@@ -78,7 +78,7 @@ export const ServiceCards = ({ selectedCountry }: any) => {
     'Add-ons': '/admin/dashboard/add-ons',
   };
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
       {services.map((service, index) => {
         const Icon = service.icon;
         return (
