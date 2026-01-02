4c1b7e500c99edba0c346fdf5f7f9c709dcbc2b5
superAman07 <rivalaman123@gmail.com>
2026-01-02 23:59:56 +0530

    fixed type error in prisma ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,11 +4,17 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
+const dbUrl = process.env.DATABASE_URL
+
+if (!dbUrl && process.env.NODE_ENV === 'production') {
+  throw new Error('Missing required environment variable: DATABASE_URL')
+}
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    datasourceUrl: process.env.DATABASE_URL,
-    log: process.env.NODE_ENV === "production" ? [] : ["query"],
-  });
+    datasources: dbUrl ? { db: dbUrl } : undefined,
+    log: process.env.NODE_ENV === 'production' ? [] : ['query'],
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
