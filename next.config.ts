76fe0937c208b40948cdef40cd3cbdb2376df7d1
superAman07 <rivalaman123@gmail.com>
2026-02-13 03:04:45 +0530

    fixed the build error on veerc

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ['@react-pdf/renderer'],
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ['@react-pdf/renderer'], 
